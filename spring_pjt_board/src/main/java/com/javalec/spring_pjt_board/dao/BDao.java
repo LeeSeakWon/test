d11a4ac501405139cf79f35c4edf433ea00f7227
lsw <lsw@DESKTOP-24J3A2N>
2018-10-25 10:56:21 +0900

    test

---
+++ spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/dao/BDao.java
@@ -0,0 +1,12 @@
+package com.javalec.spring_pjt_board.dao;
+
+import com.javalec.spring_pjt_board.dto.BDto;
+
+public class BDao {
+
+	public BDto contentView(String bid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
